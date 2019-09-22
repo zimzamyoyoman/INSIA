6870bff6424fbe4910d9cbfa4a66e72221cd3b84
Samarth Goyal <samarth_niku@yahoo.co.in>
2019-09-22 16:33:27 +0530

    Added header page with heading, subtitle, and features

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,39 @@
+// VANT 
 VANTA.WAVES({
-    el: "#example",
+    el: "#vant-background",
     color: 0x262e34
   })
+
+// TYPING TEXT 
+
+function typeEffect(element, speed) {
+	var text = element.innerHTML;
+	element.innerHTML = "";
+	
+	var i = 0;
+	var timer = setInterval(function() {
+    if (i < text.length) {
+      element.append(text.charAt(i));
+      i++;
+    } else {
+      clearInterval(timer);
+    }
+  }, speed);
+}
+
+
+// application
+var speed = 75;
+var featureSpeed=15;
+var vantSub = document.querySelector('#vant-subtitle');
+var vantFeature = document.querySelector('.vant-feature');
+var delay = vantSub.innerHTML.length * speed + speed;
+
+// type affect to header
+typeEffect(vantSub, speed);
+
+//type effect for feature 1
+setTimeout(function(){
+  vantFeature.style.display = "inline-block";
+  typeEffect(vantFeature, featureSpeed);
+}, delay);
