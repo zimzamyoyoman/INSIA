d666b0eadbdb1f7ddf7ce426a2ea82e640d8ef5c
Samarth Goyal <samarth_niku@yahoo.co.in>
2019-09-25 22:15:55 +0530

    Added typed.js in vant features

--- public/js/app.js
+++ public/js/app.js
@@ -34,7 +34,28 @@ typeEffect(vantSub, speed);
 
 // type effect for feature 1
 setTimeout(function () {
-  vantFeature.style.display = "inline-block";
-  typeEffect(vantFeature, featureSpeed);
+  var typed = new Typed('#typed', {
+    strings:
+      [
+        "Contracted vs non contracted spend distribution across category level 1.",
+        "Sales trend for last 3 years in India geography.",
+        "Top 5 campaigns this year by revenue uplift.",
+        "Best performing medical representatives by call commitments and physician coverage.",
+        "What is top of the mind awareness for Brand XYZ.",
+        "Which is the most selling category for stores near Central business district.",
+        "Average delay in cycle time for PR approval across categories.",
+        "Customer Delinquency by 30- 60 days and 60-90 days and 90 + days.",
+      ],
+    typeSpeed: 30,
+    startDelay: 1200,
+    backSpeed: 20,
+    backDelay: 500,
+    loop: true,
+    loopCount: false,
+    showCursor: false,
+    cursorChar: "|",
+  });
 }, delay);
+
+// typed.js
 
